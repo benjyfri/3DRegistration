81769552a24679a90e98b24940170fd1cbe3e55b
Benjy <minigoalwalla@gmail.com>
2023-08-03 10:57:10 +0300

    second commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('no way')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
